3592cea8d9c81c1b9753f367785a429b0a5ef5aa
Pavel Stepanek <stepasite@gmail.com>
2022-08-07 14:47:52 +0200

    wip

--- grpc/build.sh
+++ grpc/build.sh
@@ -4,4 +4,4 @@ protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
     proto/api.proto
 
-go install github.com/stepasite/webapp-grpc/grpc/proto@latest
+#go install github.com/stepasite/webapp-grpc/grpc/proto@latest
