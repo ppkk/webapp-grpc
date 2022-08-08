b7cffbe63ffb4d6660ff0a2f2cb3d26b89682694
Pavel Stepanek <stepasite@gmail.com>
2022-08-08 14:15:40 +0200

    react router, wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Login } from './pages/login';
+import { Home } from './pages/home';
 
 function App() {
-  return (
-    <div className="App">
-	Simple Web App
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Login />} />
+                                <Route path="/home" element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
