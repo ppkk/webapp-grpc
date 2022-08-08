20a8d53a1a4742668fd4ef7c6c426c11515b8a4e
Pavel Stepanek <stepasite@gmail.com>
2022-08-08 15:59:30 +0200

    login, wip

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,6 +1,44 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
-	return (<div>Login</div>);
+	const [state, setState] = React.useState({email: '', password: ''});
+	const navigate = useNavigate();
+
+	const onEmailChange = (event: any) => {
+		setState({email: event.target.value, password: state.password});
+	}
+
+	const onPasswordChange = (event: any) => {
+		setState({email: state.email, password: event.target.value});
+	}
+
+        const onLoginSubmit = (event: any) => {
+		event.preventDefault();
+		navigate("/home");
+	}
+
+	return (
+		<form onSubmit={onLoginSubmit}>
+			<h1>Login</h1>
+			<label>Email</label>
+			<input 
+				type="email"
+				value={state.email}
+				onChange={onEmailChange}
+				placeholder="Enter email address"
+			/>
+			<br />
+			<label>Password</label>
+			<input
+				type="password"
+				value={state.password}
+				onChange={onPasswordChange}
+				placeholder="Enter password"
+			/>
+			<br />
+			<button type="submit">Submit</button>
+		</form>
+	);
 }
 
