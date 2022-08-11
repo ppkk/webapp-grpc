f5c08f279130d0c103fdb0b68db95610274ae55f
Pavel Stepanek <stepasite@gmail.com>
2022-08-11 21:18:49 +0200

    todo comment added

--- frontend/src/api/news.ts
+++ frontend/src/api/news.ts
@@ -5,10 +5,10 @@ import { RepeatNewsRequest, NewsReply } from './proto/api_pb';
 export const streamNews = (count: number) => {
 	let r = new RepeatNewsRequest();
 	r.setCount(count);
+	// TODO: set GRPC_TOKEN to metadata header
 	let stream = api.streamNews(r, undefined);
 	stream.on('data', (response) => {
 		console.log(response);
 	});
-	//stream.cancel();
 }
 
